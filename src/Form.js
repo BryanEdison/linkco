51517d0c888708968a93120004ea0f0c4de7eb35
ealulema <ealulem1@binghamton.edu>
2020-09-26 17:14:46 -0400

    updated state logic for signup form

--- src/Form.js
+++ src/Form.js
@@ -17,20 +17,21 @@ export default class Form extends Component {
         //possibly antipattern to copy state from props but can't think of better solution for usecase
         this.state = {
             username: this.props.url,
-            email: '',
-            password: '',
-            confirmPassword: '',
+            email: 'email',
+            password: 'password',
+            confirmPassword: 'confirm password',
             clickedEmail: false,
             clickedUsername: false,
             clickedPassword: false,
             clickedRepeatPassword: false,
             showPasswordMismatchError: false,
+            minimumCharError: false
         }
     }
 
     componentDidMount() {
         if (!this.props.url) {
-            this.setState({username: ''})
+            this.setState({username: 'username'})
         }
     }
 
@@ -73,88 +74,100 @@ export default class Form extends Component {
     }
     
     handleClickEmail = () => {
-        this.setState({clickedEmail: true})
+        if (this.state.email === 'email') {
+            this.setState({ email: '' })
+        }
+    }
+
+    handleBlurEmail = () => {
+        if (this.state.email === '') {
+            this.setState({ email: 'email'})
+        }
     }
 
     handleClickUsername = () => {
-        //two if conditions to set state to username or typed input depending on clickk and if empty
-        if (this.state.username === 'username')
-        this.setState({username: '', clickedUsername: true})
+        //two if conditions to set state to username or typed input depending on click and if empty
+        if (this.state.username === 'username') {
+            this.setState({username: '', clickedUsername: true})
+        }
+    }
+
+    handleClickUsernameBlur = () => {
+        if (this.state.username === '') {
+            this.setState({username: 'username', clickedUsername: false})
+        }
     }
 
     handlePasswordClick = () => {
-        this.setState({ clickedPassword: true })
+        this.setState({ password: '' })
     }
 
     handleRepeatPasswordClick = () => {
-        this.setState({ clickedRepeatPassword: true})
+        if (this.state.confirmPassword === 'confirm password') {
+            this.setState({ confirmPassword: ''})
+        }
     }
 
     handleConfirmPasswordBlur = () => {
         if (this.state.password !== this.state.confirmPassword) {
             this.setState({ showPasswordMismatchError: true})
         }
+        if (this.state.confirmPassword === '') {
+            this.setState({confirmPassword: 'confirm password'})
+        }
+    }
+
+    handlePasswordBlur = () => {
+        if (this.state.password === '') {
+            this.setState({password: 'password'})
+        }
+        if (this.state.password.length < 6) {
+            this.setState({ minimumCharError: true })
+        }
     }
 
     render() {
 
-        const { email, username, password, confirmPassword, clickedPassword, clickedRepeatPassword, clickedEmail, clickedUsername, showPasswordMismatchError } = this.state
+        const { email, username, password, confirmPassword, minimumCharError, showPasswordMismatchError } = this.state
         return (
             <form onSubmit={this.handleSubmit}>
                 <div>
-                    {clickedEmail ? 
                     <StyledInput
-                    type='text'
-                    value={email}
-                    onChange={this.handleEmail}
-                    /> : <StyledInput
-                    onClick={this.handleClickEmail}
-                    onBlur={this.handleBlurEmail}
-                    type='text'
-                    value={'Email'}
-                    onChange={this.handleEmail}
-                    /> }
+                        onClick={this.handleClickEmail}
+                        onBlur={this.handleBlurEmail}
+                        type='text'
+                        value={email}
+                        onChange={this.handleEmail}
+                    />
                 </div>
                 <div>
-                    {clickedUsername ? <StyledInput
+                    <StyledInput
                     type="text"
                     value={username}
                     onChange={this.handleUsername}
                     onClick={this.handleClickUsername}
-                    /> : <StyledInput
-                    type="text"
-                    value={username || 'username'}
-                    onChange={this.handleUsername}
-                    onClick={this.handleClickUsername}
-                    />}
+                    onBlur={this.handleClickUsernameBlur}
+                    />
                 </div>
                 <div>
-                       {clickedPassword ?
-                       <StyledInput
-                        type="text"
-                        value={password}
-                        onChange={this.handlePassword}
-                       /> : 
                        <StyledInput
                        type="text"
-                       value={"Password"}
+                       value={password}
                        onChange={this.handlePassword}
                        onClick={this.handlePasswordClick}
-                      />}
+                       onBlur={this.handlePasswordBlur}
+                      />
                 </div>
+                    {minimumCharError && <StyledMismatchError>Password must be at least 6 characters
+    </StyledMismatchError> }
                 <div>
-                        {clickedRepeatPassword ? <StyledInput
+                        <StyledInput
                         type="text"
                         value={confirmPassword}
-                        onChange={this.handleConfirmPassword}
-                        onBlur={this.handleConfirmPasswordBlur}
-                        /> : <StyledInput
-                        type="text"
-                        value={"Repeat Password"}
                         onClick={this.handleRepeatPasswordClick}
                         onChange={this.handleConfirmPassword}
                         onBlur={this.handleConfirmPasswordBlur}
-                        /> }
+                        />
                 </div>
                         {showPasswordMismatchError && <StyledMismatchError>Password and Repeat Password fields must match
 </StyledMismatchError> }
