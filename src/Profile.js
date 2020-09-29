b73895b0444e6ad8c35735769321b663895a189f
ealulema <ealulem1@binghamton.edu>
2020-09-29 01:58:09 -0400

    new profile changes

--- src/Profile.js
+++ src/Profile.js
@@ -3,23 +3,57 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 
+const StyledContainer = styled.div`
+height: 100vh;
+`
 
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  min-height: 30vh;
+  margin: 50px;
+
+  @media only screen and (min-width: 440px) {
+    min-height: 100vh;
+    margin: 0px;
+  }
+`;
+
+const StyledLinkBox = styled.a`
+  width: 500px;
+  height: 30px;
+  background-color: blue;
+  margin: 10px;
+  text-decoration: none;
+  color: white;
+  border-radius: 4px;
+`
+
+const StyledHeader = styled.div`
+  font-size: 20px;
+  font-weighht: bold;
+`
 
 
 const Profile = () => {
-    const links = [{name: 'instagram', url: 'myspace.com'},{name: 'facebook', url: 'facebook.com'},{name: 'youtube', url: 'youtube.com'}]
+    const links = [{name: 'instagram', url: 'https://instagram.com/ealulema'},{name: 'facebook', url: 'https://facebook.com/ealulema'},{name: 'youtube', url: 'https://youtube.com'}]
     return (
-        <div>
-            <div>@ealulema</div>
+        <StyledContainer>
+            <StyledDiv>
+            <StyledHeader>@ealulema</StyledHeader>
             {links.map((link, idx) => 
-                (<a target='_blank' rel="noopener noreferrer" href={link.url}>
+                (<StyledLinkBox target='_blank' rel="noopener noreferrer" href={link.url}>
                 <div key={idx}>
                 <div>{link.name}</div>
                 </div>
-                </a>
+                </StyledLinkBox>
                 )
             )}
-        </div>
+            </StyledDiv>
+        </StyledContainer>
     )
 }
 
