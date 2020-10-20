9f632cc463208a8a458eb51e26b229422c3199d6
ealulema <ealulem1@binghamton.edu>
2020-10-20 10:20:04 -0400

    updated cors header

--- server.js
+++ server.js
@@ -31,16 +31,15 @@ app.use(express.static(path.join(__dirname, "build")));
 app.use(express.static("build"));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  // res.header('Access-Control-Allow-Origin', origin);
-  // res.header('Access-Control-Allow-Origin');
-  // res.header('Access-Control-Allow-Credentials', true);
-  // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  // res.header("Access-Control-Allow-Headers", "content-type");
+  // res.header("Access-Control-Allow-Origin", "*");
+  // res.header(
+  //   "Access-Control-Allow-Headers",
+  //   "Origin, X-Requested-With, Content-Type, Accept"
+  // );
+  res.header('Access-Control-Allow-Origin');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "content-type");
 
   next();
 });
