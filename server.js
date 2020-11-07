ffadc6e7bdbfe6431cba3e127031b052922bc7d5
ealulema <ealulem1@binghamton.edu>
2020-11-07 15:21:16 -0500

    changed auth/cors

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://linkco.herokuapp.com/login');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", "content-type");
+  res.header("Access-Control-Allow-Headers", "Content-Type" "Access-Control-Allow-Headers", "Authorization", "X-Requested-With");
 
   next();
 });
